7abfe95949161ee2553c6a3820edf0490e870ec7
Michal Sudwoj <mswoj61@gmail.com>
2011-01-24 16:17:19 +0100

    Changed DOCTYPE to HTML(5)

--- index.php
+++ index.php
@@ -39,14 +39,12 @@
 		header( 'Location: logout.php' ) ;
 	}
 ?>
-<!DOCTYPE html PUBLIC
- "-//W3C//DTD XHTML 1.1//EN"
- "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Absence Administration System</title>
 		<meta name="author" content="Michal Sudwoj" />
-		<meta http-equiv="Content-Type" content="text/html" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<link rel="stylesheet" type="text/css" href="settings/styles.css" />
 		
 		<script type="application/javascript" src="ajax.js" ></script>
