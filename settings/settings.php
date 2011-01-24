a5e79f94b3f93258b643d350d3aebc364b938eac
Michal Sudwoj <mswoj61@gmail.com>
2011-01-24 21:36:42 +0100

    Added default timezone.

--- settings/settings.php
+++ settings/settings.php
@@ -37,6 +37,7 @@
 	ini_set(         'memory_limit'   , '32M'  ) ;
 	error_reporting(  -1                       ) ;
 	ini_set(         'display_errors' , 'On'   ) ;
+	date_default_timezone_set( 'Europe/Zurich' ) ;
 	
 	DB::init( 'schades' ) ;
 	
